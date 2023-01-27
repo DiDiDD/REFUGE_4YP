de1613909201c088afb13ace0184056f5c620c8a
DiDiDD <dwan.subs@gmail.com>
2023-01-27 17:27:27 +0000

    delete files

--- Run/Model_train.py
+++ Run/Model_train.py
@@ -31,10 +31,10 @@ args = parser.parse_args()
 lr, batch_size, gpu_index, model_name = args.lr, args.b_s, args.gpu_index, args.model
 '''select between two model'''
 if model_name == 'unet':
-    model = build_unet()
+    model = build_unet
     model_text = 'UNET'
 elif model_name == 'sur':
-    model = model_su()
+    model = model_su
     model_text = 'swin_unetr'
 
 from torch.utils.tensorboard import SummaryWriter
